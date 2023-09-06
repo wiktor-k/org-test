cbc52aa5f084799ed559dba09eb1c138bea961cf
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2023-09-06 12:08:01 +0200

    Add script for updating tables

---
+++ update.sh
@@ -0,0 +1,12 @@
+emacs -Q --batch --eval " \
+    (progn \
+      (require 'org) \
+      (dolist (file command-line-args-left) \
+        (with-current-buffer (find-file-noselect file) \
+          (progn
+            (org-table-iterate-buffer-tables)
+            (org-save-all-org-buffers)
+          )
+        )
+      )
+    )" README.org
