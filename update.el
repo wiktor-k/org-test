20b912a95e996dc8f48c093a2c426791c4a699b1
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2023-09-06 13:50:12 +0200

    Fix elisp script

--- update.el
+++ update.el
@@ -1,7 +1,7 @@
-(progn \
- (require 'org) \
- (dolist (file command-line-args-left) \
-         (with-current-buffer (find-file-noselect file) \
+(progn
+ (require 'org)
+ (dolist (file command-line-args-left)
+         (with-current-buffer (find-file-noselect file)
                               (progn
                                 (org-table-iterate-buffer-tables)
                                 (org-save-all-org-buffers)
