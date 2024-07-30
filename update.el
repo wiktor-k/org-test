32f04551419f8f18718548b0f37da55244fa05ab
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2024-07-30 12:19:55 +0200

    Add todo

--- update.el
+++ update.el
@@ -3,6 +3,7 @@
   (dolist (file command-line-args-left)
     (with-current-buffer (find-file-noselect file)
       (progn
+        ;; TODO: org-evaluate-time-range for all lines
         (org-table-iterate-buffer-tables)
         (org-update-all-dblocks)
         (org-save-all-org-buffers)
